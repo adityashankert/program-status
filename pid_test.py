ad5d2630a4ae52e1e02b6a4f19715935cf49f519
aditya shanker tagirisa <t.a.shanker@gmail.com>
2014-09-25 16:25:39 +0530

    made it general

--- pid_test.py
+++ pid_test.py
@@ -1,9 +1,6 @@
 import os
 import platform
 import ctypes
-import mysql.connector 
-from mailer import Mailer
-from mailer import Message
 
 # GetExitCodeProcess uses a special exit code to indicate that the process is
 # still running.
@@ -39,23 +36,9 @@ def _is_pid_running_on_windows(pid):
     # process is still running.
     return is_running or exit_code.value == _STILL_ACTIVE
  
+pid= "value"
 
-cnx = mysql.connector.connect(user='root', password='',host='127.0.0.1',database='test')
-							  
-cursor = cnx.cursor()
-query = ("SELECT queue_name,pid FROM queue_pid")
-cursor.execute(query)
-for (queue_name, pid) in cursor:
-	if is_pid_running(pid)==False:
-	
-		message = Message(From="status@schoolcom.in",To="aditya@schoolcom.in,t.a.shanker@gmail.com",charset="utf-8")
-		message.Subject = "SMS queue is not running !!!"
-		message.Body = """Hi,\nSMS """+queue_name+""" is not running please look into it.\nRegards,\nStatus Team."""
-		sender = Mailer('smtp.gmail.com',587,True,'status@schoolcom.in','schoolcom')
-		sender.send(message)
-	else:
-		print "sdkfaj"
-
-cursor.close()
-
-cnx.close()
+if is_pid_running(pid)==False:
+	#action for program running
+else:
+	#action for program not running
